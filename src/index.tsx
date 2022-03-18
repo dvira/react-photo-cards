192ec2a4782ad0b7f81be28468b7541d3075893b
dvira <dvir@reallygood.co.il>
2022-03-18 22:56:29 +0200

    Support overriding Mui styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
 
-import App from './App';
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<StyledEngineProvider injectFirst>
+			<App />
+		</StyledEngineProvider>
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
